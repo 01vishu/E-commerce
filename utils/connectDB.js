fe4cc7e28915f290a72b80a234f13c136968268a
01vishu <01vishuverma@gmail.com>
2023-01-20 15:12:19 +0530

    fixed axios connection!

--- utils/connectDB.js
+++ utils/connectDB.js
@@ -1,11 +1,11 @@
 import mongoose from "mongoose";
 const connectDB = (handler) => (req, res) => {
-    mongoose.set("strictQuery", false);
-    if (mongoose.connections[0].readyState) {
-        return handler(req, res);
-    }
-    mongoose.connect(process.env.MONGO_URI);
+  mongoose.set("strictQuery", true);
+  if (mongoose.connections[0].readyState) {
     return handler(req, res);
+  }
+  mongoose.connect(process.env.MONGO_URI);
+  return handler(req, res);
 };
 
 export default connectDB;
