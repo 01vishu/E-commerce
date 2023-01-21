044f826228ded79b75597b3ac173c40831f1c00e
01vishu <01vishuverma@gmail.com>
2023-01-21 22:22:46 +0530

    Strict Query in mongoose

--- utils/connectDB.js
+++ utils/connectDB.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 const connectDB = (handler) => (req, res) => {
   if (mongoose.connections[0].readyState) {
+    mongoose.set("strictQuery", true);
     return handler(req, res);
   }
   mongoose.connect(process.env.MONGO_URI);
